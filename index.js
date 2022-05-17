5d537c98cdb04f72663000762a18044aed612b8c
Hasireddy <malikireddy.uma@gmail.com>
2022-05-17 17:39:11 +0200

    select issue

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
 
+const { Pool } = require('pg')
+const pool = new Pool({
+    host: 'tyke.db.elephantsql.com',
+    user: 'retrcbpo',
+    database: 'retrcbpo',
+    password: 'MGMQbd3OmeYsRJmpXTjsQvxxbEqYjZGi',
+    port: 5432,
+});
+
+
 app.get('/', function(req, res) {
     res.send('Hello World')
+})
+
+
+app.get('/api/recipes', function(req, res) {
+    pool
+        .query('SELECT * FROM information_schema.tables;')
+        .then(results => console.log(results))
+        .catch(err => console.error(err))
 })
 
 app.listen(3000);
