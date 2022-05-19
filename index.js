0f10a0920e934d8b487c8b72ea3d1e83562ff1dc
Hasireddy <malikireddy.uma@gmail.com>
2022-05-19 21:47:00 +0200

    test

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express')
-const app = express()
+require("dotenv").config();
+const pool = require("./db/client");
+const app = express();
+// app.get('/', function(req, res) {
+//     res.send('Hello World')
+// });
 
-const { Pool } = require('pg')
-const pool = new Pool({
-    host: 'tyke.db.elephantsql.com',
-    user: 'retrcbpo',
-    database: 'retrcbpo',
-    password: 'MGMQbd3OmeYsRJmpXTjsQvxxbEqYjZGi',
-    // port: 5432,
+
+// app.get('/api/recipes', (req, res) => {
+//     pool
+//         .query('SELECT * FROM "Preparation";')
+//         .then((result) => res.json(result.rows))
+//         .catch((err) => console.error(err))
+// });
+
+
+app.get('/api/detailedrecipe', async(req, res) => {
+    try {
+        const dbresult = await pool.query('SELECT * FROM "Preparation";')
+        console.log(dbresult.rows);
+    } catch (error) {
+        console.log('error');
+    }
 });
+// app.post('/api/preparation', (req, res) => {
+//     pool
+//         .query('INSERT INTO "Preparation" (preparation_id,description) VALUES ($1,$2);', [res.body.preparation_id, res.body.description])
+//         .then(result => res.json(result.rows))
+//         .catch((err) => console.error(err))
+// });
 
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
-})
 
-
-app.get('/api/recipes', function(req, res) {
-    pool
-        .query('SELECT * FROM Recipe;')
-        .then(results => console.log(results))
-        .catch(err => console.error(err))
-})
-
-app.listen(3000);
+app.listen(3001, () => console.log("server is listening on port 3001"));
